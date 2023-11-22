c5fba56ebdf640efeda063bd7760d1b61ca3a3fc
RoiMesh <141223735+RoiMesh@users.noreply.github.com>
2023-11-22 18:21:08 +0200

    Update negativeLoginScenarios.spec.ts

--- tests/LoginPageTests/negativeLoginScenarios.spec.ts
+++ tests/LoginPageTests/negativeLoginScenarios.spec.ts
@@ -1,29 +1,31 @@
 import { test, expect } from '@playwright/test';
-import LoginPage from '../../pages/LoginPage';
+import {pages} from '../../pages/pages'
 import { ErrorMessages } from '../../helpers/ErrorMessages';
 
 
 test.describe("Negative login Scenarios", () => {
 
-    let loginPage: LoginPage;
-
+    
     test.beforeEach(async({page}) => {
-         loginPage = new LoginPage(page);
+   
     })
 
     test ("login with locked_out_user", async({page}) => {
+        const { loginPage } = pages(page);
         await loginPage.loginToApplication(process.env.LOCKED_OUT_USER);
         await loginPage.validateErrorMessage(ErrorMessages.LOGIN_WITH_LOCKED_USER);
 
     })
 
     test ("login with incorrect user name", async({page}) => {
+        const { loginPage } = pages(page);
         await loginPage.loginToApplication("notExistUser");
         await loginPage.validateErrorMessage(ErrorMessages.INCORRECT_CREDENTIALS);
 
     })
 
     test ("login with incorrect password", async({page}) => {
+        const { loginPage } = pages(page);
         await loginPage.loginToApplication(process.env.STANDARD_USER,"1234");
         await loginPage.validateErrorMessage(ErrorMessages.INCORRECT_CREDENTIALS);
 
